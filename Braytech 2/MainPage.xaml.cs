814d38e1aa9bfef82e782825b6b0edfd6f7e11ed
Tom Chapman <chapman@live.com.au>
2018-10-08 05:53:11 +1000

    good morning

--- Braytech 2/MainPage.xaml.cs
+++ Braytech 2/MainPage.xaml.cs
@@ -17,6 +17,7 @@ using Windows.UI.Xaml.Navigation;
 using Braytech_2.Models;
 using Braytech_2.Data;
 using System.Collections.ObjectModel;
+using Braytech_2.Views;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -28,93 +29,37 @@ namespace Braytech_2
     public sealed partial class MainPage : Page
     {
 
-         ObservableCollection<Models.Item> ItemsList = new ObservableCollection<Models.Item>();
-
-
+        
         public MainPage()
         {
             this.InitializeComponent();
-
-            Request();
-
         }
 
-        private async void Request()
+        #region NavigationView event handlers
+        private void nvTopLevelNav_Loaded(object sender, RoutedEventArgs e)
         {
-            //Create an HTTP client object
-            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
-
-            //Add a user-agent header to the GET request. 
-            var headers = httpClient.DefaultRequestHeaders;
-
-            //The safe way to add a header value is to use the TryParseAdd method and verify the return value is true,
-            //especially if the header value is coming from user input.
-            string header = "ie";
-            if (!headers.UserAgent.TryParseAdd(header))
+            // set the initial SelectedItem
+            foreach (NavigationViewItemBase item in nvTopLevelNav.MenuItems)
             {
-                throw new Exception("Invalid header value: " + header);
-            }
-
-            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
-            if (!headers.UserAgent.TryParseAdd(header))
-            {
-                throw new Exception("Invalid header value: " + header);
-            }
-
-            Uri requestUri = new Uri("https://api.braytech.org/?key=5afd1373be0bc&request=vendor&hash=863940356");
-
-            //Send the GET request asynchronously and retrieve the response as a string.
-            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
-            string httpResponseBody = "";
-
-            try
-            {
-                //Send the GET request
-                httpResponse = await httpClient.GetAsync(requestUri);
-                httpResponse.EnsureSuccessStatusCode();
-                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
-
-                Render(httpResponseBody);
-
-            }
-            catch (Exception ex)
-            {
-                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
-            }
-        }
-
-        private async void Render(string json)
-        {
-            VendorData request = JsonConvert.DeserializeObject<VendorData>(json);
-
-            //help plz
-            var materialIndexes = new int[2];
-
-            foreach (var category in request.response.data[0].categories)
-            {
-                if (category.displayCategoryIndex == 1)
+                if (item is NavigationViewItem && item.Tag.ToString() == "Spider")
                 {
-                    materialIndexes = category.itemIndexes;
+                    nvTopLevelNav.SelectedItem = item;
+                    break;
                 }
             }
-
-            foreach (var item in request.response.data[0].sales)
-            {
-
-                if (materialIndexes.Contains(item.vendorItemIndex))
-                {
-                    ItemsList.Add(new Models.Item {
-                        Name = item.item.displayProperties.name.Replace("Purchase ",""),
-                        Icon = $"https://www.bungie.net{ item.item.displayProperties.icon }",
-                        Quantity = item.quantity.ToString(),
-                        Cost = $"{ item.costs[0].quantity.ToString() } {item.costs[0].displayProperties.name}"
-                    });
-                }
-                               
-
-            }
-
+            contentFrame.Navigate(typeof(Spider));
         }
+
+        private void nvTopLevelNav_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+        }
+
+        private void nvTopLevelNav_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+        }
+        #endregion
+
+        
 
     }
 }
